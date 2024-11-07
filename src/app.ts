ef4ddb401ba2a2a6676a11cbdc87c1560f917af8
Mauricio Arroyave Alzate <mauricio.arroyave@nordstrom.com>
2024-11-06 21:37:55 -0500

    restrict domains

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+const allowedOrigins = ['https://spacex-tweets-app.vercel.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+
 app.use(express.json());
 
 // Define custom format for logging
@@ -19,14 +30,13 @@ morgan.token('custom', (req: Request) => {
     method: req.method,
     url: req.originalUrl,
     timestamp: new Date().toISOString(),
-  })
-})
+  });
+});
 
-// Use morgan middleware with the custom format
 app.use(morgan(':custom'));
 
 app.get('/health', (_req, res) => {
-  res.status(200).send('Ok')
+  res.status(200).send('Ok');
 });
 
 app.use('/api/launches', launchesRoutes);
@@ -37,5 +47,5 @@ try {
     console.log(`Server is Fire at http://localhost:${PORT}`);
   });
 } catch (error) {
-  console.error("Failed to start server:", error);
+  console.error('Failed to start server:', error);
 }
